ffef7d5d1dc1c182ffd6857c6e4a76f721e791b2
Gabriel Pires <gabpiresdev@gmail.com>
2021-07-07 14:14:32 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1146736d3b6e4e04fed8902682ff196d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6cfba2b0-e30f-4673-b382-70aa4200d837'
+};
